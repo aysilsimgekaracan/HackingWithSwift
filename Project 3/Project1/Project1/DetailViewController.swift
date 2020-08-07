c3e9d574a11b9038924720953cc9e1b42046b814
Ayşıl Simge Karacan <aysilsimgekaracan@hotmail.com>
2020-08-07 22:53:06 +0300

    Hacking With Swift Project 3

---
+++ Project 3/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,66 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Ayşıl Simge Karacan on 27.07.2020.
+//  Copyright © 2020 Ayşıl Simge Karacan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var selectedImageIndex: Int?
+    var totalImage: Int?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Pictures \(String(describing: selectedImageIndex!)) of \(String(describing: totalImage!))"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        // For sharing the image
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+        
+            print("No image found")
+            return
+        }
+        
+        guard let imageName = selectedImage else {
+            print("No image name found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image, imageName], applicationActivities: [])
+         
+        
+        // activityItems -> The array of data objects on which to perform the activity.
+        // applicationActivities -> An array of UIActivity objects representing the custom services that your application supports. This parameter may be nil.
+        
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+
+
+}
